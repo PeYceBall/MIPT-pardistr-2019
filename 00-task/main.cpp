ffa286f54c5791d2e5cf244ba0e92bdc5ef7ada3
PeYceBall <31540189+PeYceBall@users.noreply.github.com>
2019-09-17 17:35:40 +0300

    Create main.cpp

---
+++ 00-task/main.cpp
@@ -0,0 +1,76 @@
+#include <mpi.h>
+#include <iostream>
+#include <numeric>
+
+int main(int argc, char** argv) {
+  MPI_Init(NULL, NULL);
+
+  int world_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+  int world_rank;
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+  if (world_rank == 0) {
+    int N;
+    std::cin >> N;
+    int* a = new int[N];
+    for (int i = 0; i < N; i++) {
+      a[i] = i + 1;
+    }
+
+    int partial_sums[world_size];
+    int batch_size = N / world_size;
+
+    // divide a into almost equal parts
+    int i = batch_size;
+    int j = 1;
+
+    while (i < N) {
+      // if the rest of a cannot be divided equally
+      // make the next part one element bigger
+      bool b = (N - i) % (world_size - j) != 0;
+      MPI_Send(&a[i], batch_size + (int)b, MPI_INT, j, j, MPI_COMM_WORLD);
+
+      i += (batch_size + (int)b);
+      j++;
+    }
+
+    partial_sums[0] = std::accumulate(a, a + batch_size, 0);
+
+    for (int i = 1; i < world_size; i++) {
+      MPI_Recv(&partial_sums[i], 1, MPI_INT, i, 1, MPI_COMM_WORLD,
+               MPI_STATUS_IGNORE);
+
+      std::cout << "Process " << i << " calculated sum of " << partial_sums[i]
+                << std::endl;
+    }
+
+    std::cout << std::endl;
+    int total_sum = std::accumulate(partial_sums, partial_sums + world_size, 0);
+    std::cout << "Total sum is " << total_sum << std::endl;
+    int real_sum = std::accumulate(a, a + N, 0);
+    std::cout << "Real sum is " << real_sum << std::endl;
+    std::cout << (real_sum == total_sum ? "Sums are equal"
+                                        : "Sums are not equal")
+              << std::endl;
+  }
+
+  if (world_rank != 0) {
+    MPI_Status status;
+    int buf_size;
+    MPI_Probe(0, world_rank, MPI_COMM_WORLD, &status);
+    MPI_Get_count(&status, MPI_INT, &buf_size);
+
+    int* buf = new int[buf_size];
+    MPI_Recv(buf, buf_size, MPI_INT, 0, world_rank, MPI_COMM_WORLD,
+             MPI_STATUS_IGNORE);
+
+    int sum = std::accumulate(buf, buf + buf_size, 0);
+    MPI_Send(&sum, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
+  }
+
+  MPI_Finalize();
+  return 0;
+}
