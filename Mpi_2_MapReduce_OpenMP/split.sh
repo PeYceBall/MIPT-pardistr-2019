1985b914f35e53192a6179ab9a0810581ad84095
Студент КАиТП 5-02 <pd79502@calc.cod.phystech.edu>
2019-11-06 02:24:10 +0300

    removed unnecessary files

--- Mpi_2_MapReduce_OpenMP/split.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ls input\ files/
-
-for f in $( ls input\ files/); do
-    echo $f
-    split -b 4096 --additional-suffix="$f" "input files/$f" "map inputs/"
-done
--- Mpi_2_MapReduce_OpenMP/test/split.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for f in $( ls ); do
-    echo $f
-    split -b 4096 --additional-suffix="$f" "$f"
-done
